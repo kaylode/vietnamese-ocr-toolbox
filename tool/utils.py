f16a1e7a894f8a5c29db98d916da914ce862b726
kaylode <kaylode@github.com>
2021-06-13 18:06:36 +0700

    add download weight from drive

--- tool/utils.py
+++ tool/utils.py
@@ -7,3 +7,17 @@ def download_weights(id_or_url, cached=None, md5=None, quiet=False):
         url = 'https://drive.google.com/uc?id={}'.format(id_or_url)
 
     return gdown.cached_download(url=url, path=cached, md5=md5, quiet=quiet)
+
+
+weight_url = {
+    "pan_resnet18_default": "1GKs-NnezTc6WN0P_Zw7h6wYzRRZdJFKl" ,
+    "pan_resnet18_sroie19": "12iWFHA0fAys2n6RyhTwNKojwe_ZyEkns" ,
+    "transformerocr_mcocr": "1qpXp_-digz2HPTGY_GPdwstzGLhjC_ot",
+    "transformerocr_default": "1aNp_vqeWRTakwQyp0ZD_1ZBZrA5cRK7d",
+    "transformerocr_config": "1xQqR9swWNCTLEa0ensPDT0HDBHTke3xT",
+    "phobert_mcocr": "1v4GQPg4Jx5FWvqJ-2k9YCxEd6iFdlXXa"
+}
+
+def download_pretrained_weights(name, cached=None):
+    return download_weights(weight_url[name], cached)
+    
