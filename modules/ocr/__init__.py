17a6b54914cc3ec89ffd7ac5a0d53b80830547e1
kaylode <kaylode@github.com>
2021-06-17 21:31:26 +0700

    fix importing

--- modules/ocr/__init__.py
+++ modules/ocr/__init__.py
@@ -1,3 +1,3 @@
 from .tool.predictor import Predictor
 from .tool.config import Cfg as Config
-from .predict import find_rotation_score, rotate_img, natural_keys
+from .predict import find_rotation_score, rotate_img
