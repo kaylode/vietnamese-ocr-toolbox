a3f2e8631fd7dcc751421afb047b15c906048cdd
Minh-Khoi Pham <pmkhoi@selab.hcmus.edu.vn>
2021-11-14 16:06:45 +0700

    crop images to png

--- modules/detection/predict.py
+++ modules/detection/predict.py
@@ -49,7 +49,7 @@ def crop_box(img, boxes, out_folder, sort=True):
         boxes = sort_box(boxes)
     
     for i, box in enumerate(boxes):
-        box_name = os.path.join(out_folder, f"{i}.jpg")
+        box_name = os.path.join(out_folder, f"{i}.png")
         
         (x1,y1),(x2,y2),(x3,y3),(x4,y4) = box
         x1,y1,x2,y2,x3,y3,x4,y4 = int(x1),int(y1),int(x2),int(y2),int(x3),int(y3),int(x4),int(y4)
